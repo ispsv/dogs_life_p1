6737426940f1bac11939747acb1e7efa50cf0efc
ispsv <ivaylospasov123@gmail.com>
2023-07-27 15:15:58 +0100

    "add test"

--- java-api/src/main/java/com/db/grad/javaapi/service/DogHandler.java
+++ java-api/src/main/java/com/db/grad/javaapi/service/DogHandler.java
@@ -1,10 +1,19 @@
 package com.db.grad.javaapi.service;
 
+import com.db.grad.javaapi.model.Dog;
 import com.db.grad.javaapi.repository.DogsRepository;
 
 public class DogHandler {
     private DogsRepository itsDogRepo;
     public DogHandler(DogsRepository itsDogRepo) {
         this.itsDogRepo = itsDogRepo;
+    }
+
+    public Long addDog(Dog dog) {
+        return this.itsDogRepo.save(dog);
+    }
+
+    public Long getNoOfDogs() {
+        return this.itsDogRepo.count();
     }
 }
--- java-api/src/test/java/com/db/grad/javaapi/service/DogsHandlerTest.java
+++ java-api/src/test/java/com/db/grad/javaapi/service/DogsHandlerTest.java
@@ -5,8 +5,7 @@ import com.db.grad.javaapi.repository.DogsRepository;
 import com.db.grad.javaapi.repository.DogsRepositoryStub;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DogsHandlerTest {
 
@@ -19,5 +18,14 @@ public class DogsHandlerTest {
     @Test
     public void add_a_dog_return_number_of_dogs_in_repo_is_one() {
         DogHandler cut = new DogHandler(itsDogRepo);
+
+        Dog theDog = new Dog();
+        theDog.setName("Bruno");
+        cut.addDog( theDog );
+
+        int expectedResult = 1;
+
+        long actualResult = cut.getNoOfDogs();
+        assertEquals( expectedResult, actualResult );
     }
 }
